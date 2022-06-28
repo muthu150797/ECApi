3d6ce110c535e22870f6b2add1a7d2ee4b95633c
muthu mani <muthumani150797@gmail.com>
2022-06-28 15:51:49 +0530

    product dto changed

--- EcAPI/Entities/ProductToReturnDTO.cs
+++ EcAPI/Entities/ProductToReturnDTO.cs
@@ -10,6 +10,8 @@ namespace EcAPI.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public int ProductTypeId { get; set; }
+        public int ProductBrandId { get; set; }
         public string PictureUrl { get; set; }
         public string Description { get; set; }
         public string ProductType { get; set; }
