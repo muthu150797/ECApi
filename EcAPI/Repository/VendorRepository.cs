4b63f7521edcc79d939a89af4ee05c558076a533
Muthu Mani <muthumani@dataguardnxt.com>
2022-07-13 15:56:20 +0530

    added vendor address

--- EcAPI/Repository/VendorRepository.cs
+++ EcAPI/Repository/VendorRepository.cs
@@ -22,18 +22,29 @@ namespace EcAPI.Repository
             {
                 var res = _context.Vendors.Add(new Vendor
                 {
-                    Email = "nm",
-                    IFSC = "",
-                    Mobile = "",
-                    AccountNumber = "",
-                    Branch = "",
-                    Name = "jsks"
+                    Name = vendorDetails.Name,
+                    Email = vendorDetails.Email,
+                    IFSC = vendorDetails.IFSC,
+                    Mobile = vendorDetails.Mobile,
+                    Active = 1,
+                    AccountNumber = vendorDetails.AccountNumber,
+                    Branch = vendorDetails.Branch,
+                    VendorAddress = new VendorAddress
+                    {
+                        FirstName = vendorDetails.VendorAddress.FirstName,
+                        LastName = vendorDetails.VendorAddress.LastName,
+                        City = vendorDetails.VendorAddress.City,
+                        Street = vendorDetails.VendorAddress.Street,
+                        State = vendorDetails.VendorAddress.State,
+                        ZipCode = vendorDetails.VendorAddress.ZipCode
+
+                    }
                 });
                 _context.SaveChanges();
             }
             catch (Exception ex)
             {
-               return 400;
+                return 400;
             }
             return 200;
         }
